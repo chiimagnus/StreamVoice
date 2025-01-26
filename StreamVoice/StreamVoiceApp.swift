6835e55dc3a9e7f385031baa9cd8cb2f06e9243d
chiimagnus <chii_magnus@outlook.com>
2025-01-26 13:57:24 +0800

    Initial Commit

---
+++ StreamVoice/StreamVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamVoiceApp.swift
+//  StreamVoice
+//
+//  Created by chii_magnus on 2025/1/26.
+//
+
+import SwiftUI
+
+@main
+struct StreamVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
